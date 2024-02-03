0fbbfb1d367aacb92cb1db454be6e5759f2ac6b7
Wesley Matos <wamk-9@hotmail.com>
2024-02-03 09:49:49 -0300

    Updated table 'companies' with default values (need php artisan migrate:fresh if installed before)

--- database/migrations/2024_02_0001_V_1_0/2024_01_19_143510_create_companies_table.php
+++ database/migrations/2024_02_0001_V_1_0/2024_01_19_143510_create_companies_table.php
@@ -14,14 +14,14 @@ return new class extends Migration
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->float('kilowatts_hour', 5, 3, true);
-            $table->float('operational_cost', 5, 2, true);
-            $table->tinyInteger('free_shipping_percentage', false, true);
-            $table->float('free_shipping_min_cost_value', 5, 2, true);
-            $table->float('free_shipping_extra_value', 4, 2, true);
-            $table->tinyInteger('marketplace_cost_percentage', false, true);
-            $table->float('marketplace_min_cost_value', 4, 2, true);
-            $table->float('marketplace_extra_cost_value', 4, 2, true);
+            $table->float('kilowatts_hour', 5, 3, true)->default(0.7);
+            $table->float('operational_cost', 5, 2, true)->default(20);
+            $table->tinyInteger('free_shipping_percentage', false, true)->default(10);
+            $table->float('free_shipping_min_cost_value', 5, 2, true)->default(25);
+            $table->float('free_shipping_extra_value', 4, 2, true)->default(0);
+            $table->tinyInteger('marketplace_cost_percentage', false, true)->default(19);
+            $table->float('marketplace_min_cost_value', 4, 2, true)->default(10);
+            $table->float('marketplace_extra_cost_value', 4, 2, true)->default(0);
             $table->timestamps();
         });
     }
